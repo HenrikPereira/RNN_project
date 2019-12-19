e0ed2fa7ad7089f80ae532843f516d2fdf97640b
HenrikPereira <henriquemiguel.pereira@gmail.com>
2019-12-19 00:56:48 +0000

    exploration

--- Scripts/data_preprocessing.py
+++ Scripts/data_preprocessing.py
@@ -1,9 +1,6 @@
 import pandas as pd
-import numpy as np
-from sklearn.preprocessing import MinMaxScaler
 
 pd.set_option('display.max_columns', None)
-pd.set_option('display.max_rows', None)
 
 test_df = pd.read_csv(r'../Data/TestDataset.csv')
 train_df = pd.read_csv(r'../Data/TrainingDataset.csv')
@@ -22,34 +19,77 @@ for col in train_df.columns:
     elif col.startswith('Outcome'):
         outc_cols.append(col)
 
+# Fill of nan values of Pre-release with the release date
+train_df['Date_2'].fillna(train_df['Date_1'], inplace=True)
 
-def drop_empty_cols(dataframe):
-    """
+# Renaming of outcomes variables
+outc_c_cols = ['Outcome_M01', 'Outcome_M02', 'Outcome_M03', 'Outcome_M04', 'Outcome_M05', 'Outcome_M06',
+               'Outcome_M07', 'Outcome_M08', 'Outcome_M09', 'Outcome_M10', 'Outcome_M11', 'Outcome_M12'
+               ]
 
-    :param dataframe:
-    :return:
-    """
-    cols = dataframe.columns.tolist()
+# create intermediate DataFrame for time series
+train_dates_outc_df = train_df[date_cols + outc_cols].rename(
+    columns=dict(zip(outc_cols, outc_c_cols))
+).reset_index().rename(         # Renaming variables for better visualization
+    columns={
+        'index': 'product',
+        'Date_2': '0',
+        'Date_1': 'D_OfRelease'}
+)
 
-    noneed = 0
-    list_noneed = []
+month = 30  # average number of days in a month
+it = 1      # iterator for adding multiple months to starting date
 
-    for c in cols:
-        if len(dataframe[c].unique()) == 1:
-            dataframe.drop(columns=[c], inplace=True)
-            cols.remove(c)
-            noneed += 1
-            list_noneed.append(c)
+# iteration to find dates for each outcome and renaming Outcomes variables to integers corresponding to months in year
+for i in outc_c_cols:
+    train_dates_outc_df[str(int(i[-2:]))] = train_dates_outc_df['0'] + it * month
+    it += 1
 
-    if noneed == 1:
-        print('\tThe {0} column was droped.'.format(list_noneed))
-    elif noneed > 1:
-        print('\t{0} columns, {1} were droped.'.format(noneed, list_noneed))
-    else:
-        print('\tNo columns were removed.')
+# defining t0 value as 0, because sales started and only in the next 30 days the cumulative sales will be known
+train_dates_outc_df['Outcome_M00'] = 0
 
+# creation of time series DataFrame by using a tidy format (melt of pandas)
+ts_train_df = pd.melt(
+    train_dates_outc_df,
+    ['product', 'D_OfRelease'],
+    [str(i) for i in range(0, 13)],
+    'Month',
+    'TimeValue'
+)
 
-minmax = MinMaxScaler()
+# Cast Month variable as int
+ts_train_df['Month'] = ts_train_df['Month'].astype('int8')
 
-train_df = train_df.fillna(0)
-train_df = pd.DataFrame(minmax.fit_transform(train_df), columns=train_df.columns)
+# adding the corresponding outcomes to its product and month
+ts_train_df['Outcome'] = pd.melt(
+    train_dates_outc_df,
+    ['product', 'D_OfRelease'],
+    ['Outcome_M00'] + outc_c_cols,
+    'Month_Outcome',
+    'Outcome'
+)['Outcome']
+
+# Sorting by Product and Month
+ts_train_df = ts_train_df.sort_values(by=['product', 'Month']).reset_index().drop('index', axis=1)
+
+# Merge product characteristics
+ts_train_df = pd.merge(
+    ts_train_df,
+    train_df[cat_cols + qnt_cols].reset_index().rename(
+        columns={'index': 'product'}
+    ),
+    on='product'
+)
+
+# creating 2 distinct scenarios of NaN filling
+# ts_train_df_zeros_version fills all NaN with 0
+# ts_train_df_z_m_version fills NaN of Outcome variable with 0's and the rest with median
+ts_train_df_zeros_version = ts_train_df.copy()
+ts_train_df_zeros_version = ts_train_df_zeros_version.fillna(0)
+ts_train_df_zeros_version = ts_train_df_zeros_version.astype('int32')
+
+ts_train_df_z_m_version = ts_train_df.copy()
+ts_train_df_z_m_version['Outcome'] = ts_train_df_z_m_version['Outcome'].fillna(0)
+ts_train_df_z_m_version = ts_train_df_z_m_version.fillna(ts_train_df_z_m_version.median())
+ts_train_df_z_m_version = ts_train_df_z_m_version.astype('int32')
+
